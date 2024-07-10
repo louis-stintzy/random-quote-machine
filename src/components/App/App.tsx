9804d2ddc380fec57ad6651072ec2008f62d3be0
louis-stintzy <louis.stintzy@gmail.com>
2024-07-10 12:25:26 +0200

    Responsive

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -34,7 +34,8 @@ function App() {
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
-    height: '100vh',
+    height: '100%',
+    minHeight: '100vh',
     backgroundColor: color.color,
     transition: 'background-color 1s ease',
   };
